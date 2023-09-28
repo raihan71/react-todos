aab9d1ae21b97ec164cea31eaafa4059c0b585aa
Raihan Nismara <raihannismara@gmail.com>
2023-09-28 16:31:53 +0700

    Fix error server

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data/db.json');
 
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
